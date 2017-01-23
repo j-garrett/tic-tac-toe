820cb17c748a1c734c2120eb092c6f0ffcf7d61c
jon <jonathan.garrett@gmail.com>
2017-01-23 14:39:07 -0800

    Implement stdin and stdout stream for read/write

---
+++ app.js
@@ -0,0 +1,16 @@
+// since this will be command line, we can't rely on DOM
+
+// We will us stdin's readable stream to get input
+// and stdout to write results
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+  var chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`data: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('end');
+});
