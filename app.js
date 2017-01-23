be7a63c44f1f6ef4020901b8f304699fe945fcab
jon <jonathan.garrett@gmail.com>
2017-01-23 14:43:39 -0800

    add some message logic

--- app.js
+++ app.js
@@ -14,3 +14,17 @@ process.stdin.on('readable', () => {
 process.stdin.on('end', () => {
   process.stdout.write('end');
 });
+
+// Track turn so we can evaluate to makr and player message
+let turn = 1;
+
+// We will evaluate marks to add vals to board
+// We will have 0 index be empty string to remove vals
+const mark = ['', 'X', 'O'];
+// We want to have a player tracker so we can notify whose turn it is
+// we will concat with tracker to make sure we put in proper val
+const player = `player${turn}`;
+
+// we want a message to tell us whose turn it is
+// AND whether they are an x or o
+const message = `It is ${player}'s turn. Play you're ${mark[turn]}, ${player}!`;
